e121609a70307c01ed064f40034f12f2aa7b3540
AnjaliRSahu <anjalisahu1996@gmail.com>
2022-09-03 08:26:38 +0530

    [Anjali]UC4-Added parttime switch file

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -2,10 +2,11 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Welcome to wage computation Program");
-            Console.WriteLine("Choose option \n 1.Employee is present or absent \t 2. Daily wage \t 3.Calculate Part Time \t4.PartTimeSwitch\t 5.Wages of a month");
+            Console.WriteLine("Choose option \n 1.Employee is present or absent \t 2. Daily wage \t 3.Calculate Part Time \t4.PartTimeSwitch\t 5.Wages of a month\n 6.Refactor using Class");
             int option=Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -24,12 +25,16 @@
                  case 5:
                     WageforMonth.CalculateMonthWage();
                     break;
+                case 6:
+                    Refactor.ComputeEmpWage();
+                    break;
                 default:
 
                     Console.WriteLine("Entered Wrong Choice ");
                     break;
 
             }
+            
         }
     }
 }
---
+++ EmployeePayroll/WagesOfMonthWhileLoop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    internal class WagesOfMonthWhileLoop
+    {
+        public static void CalculateWages()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+            while (totalEmpHrs < 100 && totalWorkingDays < 20)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);//0,1,2
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs = 4;
+                        break;
+                        case 2:
+                        empHrs = 8;
+                        break;
+                     default:
+                        empHrs = 0;
+                        break;
+                }
+                 totalEmpHrs+=empHrs;
+                Console.WriteLine("Day {0} and empHrs {1}", totalWorkingDays, totalEmpHrs);
+                }
+            int totalEmpWage = totalEmpHrs * 20;
+            Console.WriteLine();
+            Console.WriteLine("total emp wage is : {0}",totalEmpWage);
+
+
+        }
+    }
+}
